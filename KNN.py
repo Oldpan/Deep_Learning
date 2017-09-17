099a3bf4ba3e2cc2c429c427146ebdd91bddea41
oldpan <295484914@qq.com>
2017-09-17 16:47:37 +0800

    Chapter 2

--- KNN.py
+++ KNN.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 import operator
 
 
@@ -21,6 +22,50 @@ def classify0(inX, dataSet, labels, k):
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberofLines = len(arrayOLines)
+    returnMat = np.zeros((numberofLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+
+#归一化函数
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = np.zeros(np.shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - np.tile(minVals, (m, 1))
+    normDataSet = normDataSet/np.tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    hoRatio = 0.50      #hold out 10%
+    datingDataMat,datingLabels = file2matrix('machinelearninginaction/Ch02/datingTestSet2.txt')       #load data setfrom file
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print ("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if classifierResult != datingLabels[i]: errorCount += 1.0
+    print ("the total error rate is: %f" % (errorCount/float(numTestVecs)))
+    print (errorCount)
+
 
 
 
--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import KNN
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == "__main__":
-    group, labels = KNN.creatDataSet()
-    result = KNN.classify0([0, 0], group, labels, 3)
-    print(result)
+    # group, labels = KNN.creatDataSet()
+    # result = KNN.classify0([0, 0], group, labels, 3)
+    # print(result)
+
+    # datingDataMat, datingLabels = KNN.file2matrix('machinelearninginaction/Ch02/datingTestSet2.txt')
+    # print(datingDataMat)
+    # print(datingLabels)
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2],
+    #            15.0*np.array(datingLabels), 15.0*np.array(datingLabels))
+    # plt.show()
+
+    # normMat, ranges, minVals = KNN.autoNorm(datingDataMat)
+    # print(normMat)
+    # print(ranges)
+    # print(minVals)
+
+    KNN.datingClassTest()
 
 
 
