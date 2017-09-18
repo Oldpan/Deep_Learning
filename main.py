b3ac809748bcb9c2537f01691db2686711910402
oldpan <295484914@qq.com>
2017-09-18 18:18:00 +0800

    Chapter 3

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import KNN
+import tree
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 if __name__ == "__main__":
+
     # group, labels = KNN.creatDataSet()
     # result = KNN.classify0([0, 0], group, labels, 3)
     # print(result)
@@ -23,11 +26,17 @@ if __name__ == "__main__":
     # print(ranges)
     # print(minVals)
 
-    KNN.datingClassTest()
+    # KNN.datingClassTest()
 
-
-
-
+    myDat, labels = tree.CreatDataSet()
+    # myDat[0][-1] = 'maybe'
+    print(myDat)
+    # tmp1 = tree.SplitDataSet(myDat, 1, 1)
+    # tmp2 = tree.SplitDataSet(myDat, 1, 0)
+    temp = tree.ChooseBesttoSplit(myDat)
+    # tmp = tree.CalShannonEnt(myDat)
+    # print(tmp1, '\n', tmp2)
+    print(temp)
 
 
 
---
+++ tree.py
@@ -0,0 +1,72 @@
+from math import log
+import operator
+
+
+def CalShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+def CreatDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+def SplitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def ChooseBesttoSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = CalShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = SplitDataSet(dataSet, i ,value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * CalShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+def MajorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def CreateTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList
